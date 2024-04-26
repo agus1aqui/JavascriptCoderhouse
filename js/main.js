b0665f767c1c564eead7abab2e13fe1fa5e484d8
Agustin Aquino <aquinoagustin20@outlook.com>
2024-04-25 22:18:56 -0300

    segunda entraga

--- js/main.js
+++ js/main.js
@@ -1,91 +1,137 @@
-// Eleccion de colores 
-const precioColores = {
-    "Negro Jet Metalizado": 5018.00,
-    "Gris GT": 4060.00,
-    "Azul Genciana Metalizado": 5522.00
-};
-
-let seleccionColores = prompt("Elige un color: \n1. Negro Jet Metalizado\n2. Gris GT\n3. Azul Genciana Metalizado");
-
-switch (seleccionColores) {
-    case "1":
-        seleccionColores = "Negro Jet Metalizado";
-        break;
-    case "2":
-        seleccionColores = "Gris GT";
-        break;
-    case "3":
-        seleccionColores = "Azul Genciana Metalizado";
-        break;
-    default:
-        console.log("Selección invalida. Por favor, elige una opción valida.");
+class Items {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        //disponibilidad? 
+    }
 }
-
-if (precioColores[seleccionColores]) {
-    console.log("El precio del color seleccionado es: $" + precioColores[seleccionColores]);
-} else {
-    console.log("Selección invalida. Por favor, elige una opción valida.");
+//colores
+//objetos 
+const color1 = new Items("Negro Jet Metalizado", 5018.00);
+const color2 = new Items("Gris GT", 4060.00);
+const color3 = new Items("Azul Genciana Metalizado", 5522.00);
+//funciones
+function mostrarSeleccionColor() {
+    let seleccionColores = parseInt(prompt("Elige un color: \n1. Negro Jet Metalizado \n2. Gris GT \n3. Azul Genciana Metalizado"));
+    if (isNaN(seleccionColores)) {
+        alert("Dato no válido, por favor ingrese nuevamente");
+        return mostrarSeleccionColor();
+    } else if (seleccionColores > 3 || seleccionColores < 1) {
+        alert("Dato no válido, por favor ingrese nuevamente");
+        return mostrarSeleccionColor();
+    } else {
+        return seleccionColores;
+    }
 }
-
-
-// Eleccion de llantas
-const precioLlantas = {
-    "Llantas Carrera de 19 y 20 pulgadas": 0.00,
-    "Llantas Carrera Classic de 20 y 21 pulgadas": 8900.00,
-    "Llantas Turbo de 20 y 21 pulgadas": 9500.00
-};
-
-let seleccionLlantas = prompt("Elige unas llantas: \n1. Llantas Carrera de 19 y 20 pulgadas\n2. Llantas Carrera Classic de 20 y 21 pulgadas\n3. Llantas Turbo de 20 y 21 pulgadas");
-
-switch (seleccionLlantas) {
-    case "1":
-        seleccionLlantas = "Llantas Carrera de 19 y 20 pulgadas";
-        break;
-    case "2":
-        seleccionLlantas = "Llantas Carrera Classic de 20 y 21 pulgadas";
-        break;
-    case "3":
-        seleccionLlantas = "Llantas Turbo de 20 y 21 pulgadas";
-        break;
-    default:
-        console.log("Seleccion invalida. Por favor, elige una opcion valida.");
+function menuSeleccionColores() {
+    const seleccionColores = mostrarSeleccionColor();
+    let colorElegido;
+    switch (seleccionColores) {
+        case 1:
+            colorElegido = (color1.nombre + " con un valor de: $ " + color1.precio);
+            break;
+        case 2:
+            colorElegido = (color2.nombre + " con un valor de: $ " + color2.precio);
+            break;
+        case 3:
+            colorElegido = (color3.nombre + " con un valor de: $ " + color3.precio);
+            break;
+        default:
+            console.log("Selección no válida");
+            return;
+    }
+    alert("El color elegido es: " + colorElegido)
 }
+menuSeleccionColores();
 
-if (precioLlantas[seleccionLlantas]) {
-    console.log("El precio de las llantas seleccionadas es: $" + precioLlantas[seleccionLlantas]);
-} else {
-    console.log("Seleccion invalida. Por favor, elige una opcion valida");
+
+
+//lantas
+//objetos
+const llantas1 = new Items("Llantas Carrera de 19 y 20 pulgada", 0.00);
+const llantas2 = new Items("Llantas Carrera Classic de 20 y 21 pulgadas", 8900.00);
+const llantas3 = new Items("Llantas Turbo de 20 y 21 pulgadas", 9500.00);
+//funciones
+function mostrarSeleccionLlantas() {
+    let seleccionLlantas = parseInt(prompt("Elige un color: \n1. Llantas Carrera de 19 y 20 pulgadas \n2. Llantas Carrera Classic de 20 y 21 pulgadas \n3. Llantas Turbo de 20 y 21 pulgadas"));
+    if (isNaN(seleccionLlantas)) {
+        alert("Dato no válido, por favor ingrese nuevamente");
+        return mostrarSeleccionLlantas();
+    } else if (seleccionLlantas > 3 || seleccionLlantas < 1) {
+        alert("Dato no válido, por favor ingrese nuevamente");
+        return mostrarSeleccionLlantas();
+    } else {
+        return seleccionLlantas;
+    }
 }
-
-
-//eleccion iluminacion
-const precioIluminacion = {
-    "Faros principales LED Dynamic Light System Plus": 2000.00,
-    "Faros principales ED Matrix Dynamic Light System Plus": 5000.00,
-    "Faros principales LED tintados Matrix Beam Dynamic Light System Plus": 14000.00
-};
-
-let seleccionIluminacion = prompt("Elige el sistema de iluminacion: \n1.Faros principales LED Dynamic Light System Plus \n2. Faros principales ED Matrix Dynamic Light System Plus\n3. Faros principales LED tintados Matrix Beam Dynamic Light System Plus");
-
-switch (seleccionIluminacion) {
-    case "1":
-        seleccionIluminacion = "Faros principales LED Dynamic Light System Plus";
-        break;
-    case "2":
-        seleccionIluminacion = "Faros principales ED Matrix Dynamic Light System Plus";
-        break;
-    case "3":
-        seleccionIluminacion = "Faros principales LED tintados Matrix Beam Dynamic Light System Plus";
-        break;
-    default:
-        console.log("Seleccion invalida. Por favor, elige una opcion valida.");
+function menuSeleccionLlantas() {
+    const seleccionLlantas = mostrarSeleccionLlantas();
+    let llantasElegido;
+    switch (seleccionLlantas) {
+        case 1:
+            llantasElegido = "Llantas Carrera de 19 y 20 pulgadas";
+            break;
+        case 2:
+            llantasElegido = "Llantas Carrera Classic de 20 y 21 pulgadas";
+            break;
+        case 3:
+            llantasElegido = "Llantas Turbo de 20 y 21 pulgadas";
+            break;
+        default:
+            console.log("Selección no válida");
+            return;
+    }
+    alert("Las llantas elegidas son: " + llantasElegido)
 }
+menuSeleccionLlantas();
 
-if (precioIluminacion[seleccionIluminacion]) {
-    console.log("El precio del sistema de uliminacion elegido es: $" + precioIluminacion[seleccionIluminacion]);
-} else {
-    console.log("Seleccion invalida. Por favor, elige una opcion valida");
+
+
+//uliminacion
+//objetos
+const iluminacion1 = new Items("Faros principales LED Dynamic Light System Plus", 2000.00);
+const iluminacion2 = new Items("Faros principales ED Matrix Dynamic Light System Plus", 5000.00);
+const iluminacion3 = new Items("Faros principales Led tintados Matrix Beam Dynamic Light System Plus", 14000.00);
+//funciones
+function mostrarSeleccionIluminacion() {
+    let seleccionIluminacion = parseInt(prompt("Elige un color: \n1. Faros principales LED Dynamic Light System Plus \n2. Faros principales ED Matrix Dynamic Light System Plus \n3. Faros principales LED tintados Matrix Beam Dynamic Light System Plus"));
+    if (isNaN(seleccionIluminacion)) {
+        alert("Dato no válido, por favor ingrese nuevamente");
+        return mostrarSeleccionIluminacion();
+    } else if (seleccionIluminacion > 3 || seleccionIluminacion < 1) {
+        alert("Dato no válido, por favor ingrese nuevamente");
+        return mostrarSeleccionIluminacion();
+    } else {
+        return seleccionIluminacion;
+    }
 }
+function menuSeleccionIluminacion() {
+    const seleccionIluminacion = mostrarSeleccionIluminacion();
+    let iluminacionElegido;
+    switch (seleccionIluminacion) {
+        case 1:
+            iluminacionElegido = "Faros principales LED Dynamic Light System Plus";
+            break;
+        case 2:
+            iluminacionElegido = "Faros principales ED Matrix Dynamic Light System Plus";
+            break;
+        case 3:
+            iluminacionElegido = "Faros principales LED tintados Matrix Beam Dynamic Light System Plus";
+            break;
+        default:
+            console.log("Selección no válida");
+            return;
+    }
+    alert("La iluminacion elegida es: " + iluminacionElegido)
+}
+menuSeleccionIluminacion();
+
+
+
+
+
+
+
 
 
 // Total
@@ -97,6 +143,7 @@ function sumar(precioBase, precioColores, precioLlantas, precioIluminacion) {
 
 function mostrar(mensaje) {
     alert("Costo total: USD " + mensaje);
+    //sumar iva?
 }
 
 const precioSeleccionadoColores = precioColores[seleccionColores];
@@ -107,3 +154,4 @@ mostrar(resultado);
 
 
 
+//falta carregir los precios
